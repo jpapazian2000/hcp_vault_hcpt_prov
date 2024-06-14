9b8ac3125999f596f6238e098a2c55f355813966
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-14 18:24:52 +0200

    new commit

---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "hvn_id" {
+  description = "The ID of the HCP HVN."
+  type        = string
+  default     = "learn-hcp-vault-hvn"
+}
+
+variable "cluster_id" {
+  description = "The ID of the HCP Vault cluster."
+  type        = string
+  default     = "sanofi-vault-cluster"
+}
+
+variable "region" {
+  description = "The region of the HCP HVN and Vault cluster."
+  type        = string
+  default     = "francecentral"
+}
+
+variable "cloud_provider" {
+  description = "The cloud provider of the HCP HVN and Vault cluster."
+  type        = string
+  default     = "azure"
+}
+
+variable "tier" {
+  description = "Tier of the HCP Vault cluster. Valid options for tiers."
+  type        = string
+  default     = "dev"
+}
