6a63b9708a54cbba79ca58a4a7038f2c93b8b317
jerome papazian <j_papazian2000@yahoo.fr>
2024-06-30 22:15:14 +0200

    prov with audit export

--- variables.tf
+++ variables.tf
@@ -32,3 +32,27 @@ variable "tier" {
   type        = string
   default     = "starter"
 }
+variable "audit-user" {
+  description = "username of the audit user for audit logs export in grafana"
+  type        = string
+}
+#variable "audit-pwd" {
+  #description = "pasword of the audit user for audit logs export in grafana"
+  #type        = string
+#}
+variable "audit-url" {
+  description = "endpoint of the audit log in grafana"
+  type        = string
+}
+variable "metric-user" {
+  description = "username of the  user for metrics export in grafana"
+  type        = string
+}
+variable "metric-pwd" {
+  description = "pasword of the user for metrics export in grafana"
+  type        = string
+}
+variable "metric-url" {
+  description = "metrics endpoint in grafana"
+  type        = string
+}
